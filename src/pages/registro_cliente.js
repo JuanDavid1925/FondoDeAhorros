1710ebe3d7d94bcaf842968f039937e684e5b00b
karenmoreno17 <karen.moreno@correounivalle.edu.co>
2022-11-14 16:44:37 -0500

    Actualización registros

--- src/pages/registro_cliente.js
+++ src/pages/registro_cliente.js
@@ -27,101 +27,102 @@ export default function Registro_cliente() {
             <p className="mt-3 text-gray-600 dark:text-gray-300">
               Regístrate como cliente y contacta a un asociado para empezar a disfrutar de nuestros servicios.
             </p>
-
-            <Formik
-              initialValues=
-              {{
-                documento: "",
-                nombres: "",
-                apellidos: "",
-                telefono: "",
-                contrasena: "",
-                confirContrasena: "",
-                documento_asociado: ""
-              }}
-              onSubmit={handleSubmit}
-            >
-              {
-                ({ handleChange, handleSubmit }) => (
-                  <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
-                    <div>
-                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nombre (s) </label>
-                      <input
-                        onChange={handleChange}
-                        id="nombres"
-                        name="nombres"
-                        type="text"
-                        placeholder="Ingrese su(s) nombre(s)"
-                        className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
-                      />
-                    </div>
-                    <div>
-                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Apellido (s)</label>
-                      <input
-                        onChange={handleChange}
-                        id="apellidos"
-                        name="apellidos"
-                        type="text"
-                        placeholder="Ingrese su(s) apellido(s)"
-                        className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-                    <div>
-                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Documento de identificación</label>
-                      <input
-                        onChange={handleChange}
-                        id="documento"
-                        name="documento"
-                        type="text"
-                        placeholder="Ingrese tu documento"
-                        className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-                    <div>
-                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Teléfono</label>
-                      <input
-                        onChange={handleChange}
-                        id="telefono"
-                        name="telefono"
-                        type="text"
-                        placeholder="Ingrese su número de contacto"
-                        className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-                    <div>
-                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Identificación del asociado a vincular</label>
-                      <input
-                        onChange={handleChange}
-                        id="documento_asociado"
-                        name="documento_asociado"
-                        type="text"
-                        placeholder="Ingrese el número del asociado"
-                        className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-                    <div>
-                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Contraseña</label>
-                      <input
-                        onChange={handleChange}
-                        id="contrasena"
-                        name="contrasena"
-                        type="password"
-                        placeholder="Ingrese una contraseña"
-                        className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-                    <div>
-                      <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Confirmar contraseña</label>
-                      <input
-                        onChange={handleChange}
-                        id="confirContrasena"
-                        name="confirContrasena"
-                        type="password"
-                        placeholder="Ingrese nuevamente su contraseña"
-                        className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                    </div>
-                    <button type="submit" className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide bg-blue-400 capitalize rounded-md border-blue-400 border-2 text-white hover:text-white font-semibold hover:shadow-[inset_20rem_0_0_0] hover:shadow-blue-600 duration-[400ms,800ms] transition-[color,box-shadow]">
-                      <span><label className="mt-4 text-white-500 white:text-white-400 cursor-pointer">Registrarse</label></span>
-                    </button>
-                  </form>
-                )
-              }
-            </Formik>
+            <div>
+              <Formik
+                initialValues=
+                {{
+                  documento: "",
+                  nombres: "",
+                  apellidos: "",
+                  telefono: "",
+                  contrasena: "",
+                  confirContrasena: "",
+                  documento_asociado: ""
+                }}
+                onSubmit={handleSubmit}
+              >
+                {
+                  ({ handleChange, handleSubmit }) => (
+                    <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+                      <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nombre (s) </label>
+                        <input
+                          onChange={handleChange}
+                          id="nombres"
+                          name="nombres"
+                          type="text"
+                          placeholder="Ingrese su(s) nombre(s)"
+                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                      </div>
+                      <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Apellido (s)</label>
+                        <input
+                          onChange={handleChange}
+                          id="apellidos"
+                          name="apellidos"
+                          type="text"
+                          placeholder="Ingrese su(s) apellido(s)"
+                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                      </div>
+                      <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Documento de identificación</label>
+                        <input
+                          onChange={handleChange}
+                          id="documento"
+                          name="documento"
+                          type="text"
+                          placeholder="Ingrese tu documento"
+                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                      </div>
+                      <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Teléfono</label>
+                        <input
+                          onChange={handleChange}
+                          id="telefono"
+                          name="telefono"
+                          type="text"
+                          placeholder="Ingrese su número de contacto"
+                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                      </div>
+                      <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Identificación del asociado a vincular</label>
+                        <input
+                          onChange={handleChange}
+                          id="documento_asociado"
+                          name="documento_asociado"
+                          type="text"
+                          placeholder="Ingrese el número del asociado"
+                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                      </div>
+                      <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Contraseña</label>
+                        <input
+                          onChange={handleChange}
+                          id="contrasena"
+                          name="contrasena"
+                          type="password"
+                          placeholder="Ingrese una contraseña"
+                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                      </div>
+                      <div>
+                        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Confirmar contraseña</label>
+                        <input
+                          onChange={handleChange}
+                          id="confirContrasena"
+                          name="confirContrasena"
+                          type="password"
+                          placeholder="Ingrese nuevamente su contraseña"
+                          className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                      </div>
+                      <button type="submit" className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide bg-blue-400 capitalize rounded-md border-blue-400 border-2 text-white hover:text-white font-semibold hover:shadow-[inset_20rem_0_0_0] hover:shadow-blue-600 duration-[400ms,800ms] transition-[color,box-shadow]">
+                        <span><label className="mt-4 text-white-500 white:text-white-400 cursor-pointer">Registrarse</label></span>
+                      </button>
+                    </form>
+                  )
+                }
+              </Formik>
+            </div>
 
           </div>
         </div>
