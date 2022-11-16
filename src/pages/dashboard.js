c4405c53ac64d0ebea8bc409681475dd5367927b
karenmoreno17 <karen.moreno@correounivalle.edu.co>
2022-11-16 01:09:56 -0500

    Actualización Dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -60,15 +60,12 @@ export default function Dashboard() {
                     </ul>
                 </div>
                 <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-
-                    <a href="./login">
-                        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path className="group-hover:text-cyan-600" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                            </svg>
-                            <span className="group-hover:text-cyan-600">Cerrar sesión</span>
-                        </button>
-                    </a>
+                    <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path className="group-hover:text-cyan-600" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                        </svg>
+                        <span className="group-hover:text-cyan-600">Cerrar sesión</span>
+                    </button>
                 </div>
             </aside>
             <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
