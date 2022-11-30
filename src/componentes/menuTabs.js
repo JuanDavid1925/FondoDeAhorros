1854415edfbbeb3cdec2156d4c0a2611ad348a79
karenmoreno17 <karen.moreno@correounivalle.edu.co>
2022-11-30 02:55:32 -0500

    Añade Tabs al panel de modificación de usuario

---
+++ src/componentes/menuTabs.js
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled'
+
+export const TabHead = styled("div")`
+  border-bottom: 1px solid blue;
+  display: flex;
+`
+export const TabContainer = styled("div")`
+  width: 100%;
+  height: 100%;
+`
+
+export const TabBody = styled("div")`
+  height: 100%;
+  width: 100%;
+`
+export const Tab = styled("div")`
+  padding: 0.5em;
+  background: ${({ selected }) => (selected ? "#60a5fa" : "#ebedf0")};
+  color: ${({ selected }) => (selected ? "white" : "#4b5563")}
+`
--- src/componentes/panelAux.js
+++ src/componentes/panelAux.js
@@ -1,19 +1,13 @@
 
-import { Formik, Form, ErrorMessage, Field } from 'formik';
-import * as Yup from 'yup';
-import { useRouter } from 'next/router'
-import { useCallback, useContext, useEffect, useState } from "react"
-import Context from "/src/context/userContext"
-
 
 export default function PanelAux() {
 
-    const router = useRouter()
-    const { userData } = useContext(Context)
-    return (
 
-        <div >
+  return (
+    <div>
 
-        </div >
-    )
+    </div >
+
+  )
+
 }
