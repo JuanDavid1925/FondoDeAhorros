bae4ff7fc50578dfbce797d3c924d936887d6e90
karenmoreno17 <karen.moreno@correounivalle.edu.co>
2022-11-29 22:34:58 -0500

    Actualización del  panel de modificación de usuario

--- src/componentes/panelModiUsuario.js
+++ src/componentes/panelModiUsuario.js
@@ -61,7 +61,7 @@ export default function PanelModiUsuario() {
                       id="nombres"
                       name="nombres"
                       type="text"
-                      placeholder="Ingrese su(s) nombre(s)"
+                      placeholder="Ingrese el nuevo nombre"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                   </div>
                   <div>
@@ -71,7 +71,7 @@ export default function PanelModiUsuario() {
                       id="apellidos"
                       name="apellidos"
                       type="text"
-                      placeholder="Ingrese su(s) apellido(s)"
+                      placeholder="Ingrese el nuevo apellido"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                   </div>
                   <div>
@@ -81,7 +81,7 @@ export default function PanelModiUsuario() {
                       id="telefono"
                       name="telefono"
                       type="text"
-                      placeholder="Ingrese su número de contacto"
+                      placeholder="Ingrese el nuevo número de contacto"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                   </div>
                   <div>
@@ -91,7 +91,7 @@ export default function PanelModiUsuario() {
                       id="correo"
                       name="correo"
                       type="email"
-                      placeholder="Ingrese su correo electrónico"
+                      placeholder="Ingrese el nuevo correo electrónico"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                   </div>
                   <div>
@@ -101,7 +101,7 @@ export default function PanelModiUsuario() {
                       id="ocupacion"
                       name="ocupacion"
                       type="text"
-                      placeholder="Ingrese su ocupación"
+                      placeholder="Ingrese la nueva ocupación"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                   </div>
 
@@ -112,7 +112,7 @@ export default function PanelModiUsuario() {
                       id="ciudad"
                       name="ciudad"
                       type="text"
-                      placeholder="Ingrese su ciudad"
+                      placeholder="Ingrese la nueva ciudad"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                   </div>
                   <div>
@@ -122,7 +122,7 @@ export default function PanelModiUsuario() {
                       id="direccion"
                       name="direccion"
                       type="text"
-                      placeholder="Ingrese su dirección"
+                      placeholder="Ingrese la nueva dirección"
                       className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                   </div>
                   <br></br>
