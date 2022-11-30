47930e286e28f1da26c2774778e8563fe4574bcf
karenmoreno17 <karen.moreno@correounivalle.edu.co>
2022-11-30 12:18:11 -0500

    Actualización Módulo de modificación de cliente

--- src/componentes/panelModiUsuario.js
+++ src/componentes/panelModiUsuario.js
@@ -53,7 +53,7 @@ const PanelModiUsuario = ({ router }) => {
                   {
                     ({ handleChange, handleSubmit, errors, touched }) => (
                       <div>
-                        <div className="px-8 mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+                        <div className="px-8 ml-28 mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
                           <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
                             <div>
                               <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Documento a buscar</label>
@@ -161,7 +161,98 @@ const PanelModiUsuario = ({ router }) => {
                 </Formik>
               </div >
             </React.Fragment>}
-          {isTabTwo && <React.Fragment>This is tab two content</React.Fragment>}
+          {isTabTwo && <React.Fragment>
+            <div >
+              <Formik
+                initialValues=
+                {{
+                  nombres: "",
+                  apellidos: "",
+                  telefono: "",
+                  correo: "",
+                  ocupacion: "",
+                  ciudad: "",
+                  direccion: "",
+                }}
+
+
+                onSubmit={fields => {
+                  handleSubmit(fields)
+                }}
+
+              >
+                {
+                  ({ handleChange, handleSubmit, errors, touched }) => (
+                    <div>
+                      <div className="px-8 mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+                        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
+                          <div>
+                            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Documento a buscar</label>
+                            <input
+                              onChange={handleChange}
+                              id="documentoB"
+                              name="documentoB"
+                              type="text"
+                              placeholder="Ingrese el documento del cliente"
+                              className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                          </div>
+                          <div className="form-group flex">
+                            <button className="btn-primary flex items-center justify-between  px-6 py-5 text-sm tracking-wide bg-blue-400 capitalize rounded-md border-blue-400 border-2 text-white font-semibold">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 15.75l-2.489-2.489m0 0a3.375 3.375 0 10-4.773-4.773 3.375 3.375 0 004.774 4.774zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                              </svg>
+                              <span><label className="mt-4 text-white-500 white:text-white-400 cursor-pointer"> Buscar</label></span>
+                            </button>
+                          </div>
+                          <div>
+                            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Nombre(s) </label>
+                            <input
+                              onChange={handleChange}
+                              id="nombres"
+                              name="nombres"
+                              type="text"
+                              placeholder="Ingrese el nuevo nombre"
+                              className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                          </div>
+                          <div>
+                            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Apellido(s)</label>
+                            <input
+                              onChange={handleChange}
+                              id="apellidos"
+                              name="apellidos"
+                              type="text"
+                              placeholder="Ingrese el nuevo apellido"
+                              className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                          </div>
+                          <div>
+                            <label className="block mb-2 text-sm text-gray-600 dark:text-gray-200">Teléfono</label>
+                            <input
+                              onChange={handleChange}
+                              id="telefono"
+                              name="telefono"
+                              type="text"
+                              placeholder="Ingrese el nuevo número de contacto"
+                              className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                          </div>
+
+                          <br></br>
+                          <div className="form-group flex ">
+                            <button type="submit"
+                              onClick={() => useEffect}
+                              className="btn-primary flex items-center justify-between  px-6 py-5 text-sm tracking-wide bg-blue-400 capitalize rounded-md border-blue-400 border-2 text-white font-semibold">
+                              <span><label className="mt-4 text-white-500 white:text-white-400 cursor-pointer">Modificar datos</label></span>
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  )
+
+                }
+              </Formik>
+            </div >
+
+          </React.Fragment>}
         </TabBody>
       </TabContainer>
     </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import "tailwindcss/tailwind.css"
 import { UserContextProvider } from '/src/context/userContext'
+import "/src/styles/Home.module.css"
 
 function MyApp({ Component, pageProps }) {
   return <> <UserContextProvider> <Component {...pageProps} /> </UserContextProvider> </>
