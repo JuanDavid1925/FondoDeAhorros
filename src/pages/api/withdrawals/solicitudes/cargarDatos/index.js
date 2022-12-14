3c3dc464fddd8d4efeb5cf65ee863acd8faef566
AlivUV <alvaro.portocarrero@correounivalle.edu.co>
2022-12-14 11:24:17 -0500

    Avances api solicitudes.

--- src/pages/api/withdrawals/solicitudes/cargarDatos/index.js
+++ src/pages/api/withdrawals/solicitudes/cargarDatos/index.js
@@ -1,12 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { verify } from 'jsonwebtoken'
 
+import { conn } from '/src/utils/database'
+
 /**
  * @param {NextApiRequest} req
  * @param {NextApiResponse} res
 */
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (req, res) => {
+export default async (req, res) => {
   const { jwt } = req.cookies
 
   try {
@@ -15,6 +17,10 @@ export default (req, res) => {
     }
 
     const userData = verify(jwt, 'DSII')
+
+    const query1 = ''
+    
+    const res1 = await conn.query(query1)
 
     return res.status(200).json({ estado: 200, mensaje: 'Datos obtenidos correctamente.', user: { nombres: userData.nombres, apellidos: userData.apellidos, tipo: userData.tipo, documento: userData.documento } })
   } catch ({ message }) {
