18adebc85de12ac282802e132ed9c3c646cf3f6c
AlivUV <alvaro.portocarrero@correounivalle.edu.co>
2022-12-14 11:55:33 -0500

    Cargar datos para solicitud de retiro.

--- src/pages/api/withdrawals/solicitudes/cargarDatos/index.js
+++ src/pages/api/withdrawals/solicitudes/cargarDatos/index.js
@@ -18,11 +18,29 @@ export default async (req, res) => {
 
     const userData = verify(jwt, 'DSII')
 
-    const query1 = ''
-    
-    const res1 = await conn.query(query1)
+    const query1 = `
+      SELECT documento_usuario ${(userData.tipo === 'Asociado' ? ', correo_asociado, monto_total_asociado' : '') }
+      FROM usuarios ${ (userData.tipo === 'Admin')
+        ? ''
+        : `JOIN ${(userData.tipo === 'Asociado') ? 'asociados' : 'clientes'}
+          ON usuarios.documento_usuario = ${(userData.tipo === 'Asociado') ? 'documento_asociado' : 'documento_cliente'}`
+      }
+      WHERE documento_usuario = ${userData.documento};`
 
-    return res.status(200).json({ estado: 200, mensaje: 'Datos obtenidos correctamente.', user: { nombres: userData.nombres, apellidos: userData.apellidos, tipo: userData.tipo, documento: userData.documento } })
+    const resp1 = await conn.query(query1)
+
+    if (!resp1.rowcount)
+      return res.status(400).json({ estado: 404, mensaje: 'Usuario no encontrado.' })
+
+    return res.status(200).json({ 
+      estado: 200, 
+      mensaje: 'Datos obtenidos correctamente.', 
+      datos: { 
+        documento: userData.documento, 
+        correo: resp1.rows[0].correo_asociado,
+        saldo: resp1.rows[0].monto_total_asociado
+      } 
+    })
   } catch ({ message }) {
     return res.status(401).json({ estado: 401, mensaje: message })
   }
