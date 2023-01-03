35945d948ef11374b91113f7a5a97832d72e7653
AlivUV <alvaro.portocarrero@correounivalle.edu.co>
2023-01-03 10:56:08 -0500

    Cambio panel pago cuotas.

--- src/componentes/dashboard/asociado/panelPagoCuotas/modalCuotaMensual.js
+++ src/componentes/dashboard/asociado/panelPagoCuotas/modalCuotaMensual.js
@@ -1,5 +1,5 @@
-import { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useRef, useState } from 'react'
 
 export default function PagoCuotaMensualExitoso({ children, onClose }) {
   const [open, setOpen] = useState(true)
