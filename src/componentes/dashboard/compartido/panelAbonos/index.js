7b8e155699cdbc0970d30d6d9fbcf205db9dbedb
karenmoreno17 <karen.moreno@correounivalle.edu.co>
2023-01-04 00:58:28 -0500

    Panel de Abonos del cliente

--- src/componentes/dashboard/compartido/panelAbonos/index.js
+++ src/componentes/dashboard/compartido/panelAbonos/index.js
@@ -7,10 +7,15 @@ import $ from "jquery"
 import { TabHead, TabContainer, TabBody, Tab } from "/src/componentes/dashboard/menuTabs"
 import Header_asociado from "/src/componentes/dashboard/compartido/header_asociado"
 import useSaves from "/src/hooks/useSaves"
+import PagoCuotaExitoso from "./modalCuota"
 
-const PanelTarjeta = ({ router }) => {
+const PanelAbonos = ({ router }) => {
+
   const { pagoMensual } = useSaves()
-  const [estado, setEstado] = useState()
+  const [showModal, setShowModal] = useState(false)
+
+  const handleClose = useCallback(() => { setShowModal(false) }, [])
+
 
   const {
     query: { tab }
@@ -20,7 +25,7 @@ const PanelTarjeta = ({ router }) => {
   const isTabTwo = tab === "2"
 
   const handleSubmit = useCallback(() => {
-    pagoMensual($("#cantidad_pago").val(), setEstado)
+    pagoMensual($("#cantidadPagar").val(), setEstado)
   }, [pagoMensual])
 
   return (
@@ -30,12 +35,12 @@ const PanelTarjeta = ({ router }) => {
         <TabHead>
           <Tab selected={isTabOne}>
             <Link href={{ query: { tab: "1" } }}>
-              <a>Pago de cuota mínima</a>
+              <a>Pago de cuotas</a>
             </Link>
           </Tab>
           {/*<Tab selected={isTabTwo}>
             <Link href={{ query: { tab: "2" } }}>
-              <a>Pago de cuota de manejo</a>
+              <a>Pago</a>
             </Link>
   </Tab>*/}
         </TabHead>
@@ -46,8 +51,11 @@ const PanelTarjeta = ({ router }) => {
                 <Formik
                   initialValues=
                   {{
-                    cuotaMin: "",
-                    cuotaManejo: ""
+                    cardholder: "",
+                    cardNo: "",
+                    fechaDeExpiración: "",
+                    code: "",
+                    cantidadPagar: "",
                   }}
 
                   onSubmit={handleSubmit}
@@ -57,20 +65,20 @@ const PanelTarjeta = ({ router }) => {
                     ({ handleChange, handleSubmit, errors, touched }) => (
                       <div className="grid sm:px-10 lg:grid-cols-1 ">
                         <br></br>
-                        <p className="text-gray-700 sm:text-xl">Aquí puede realizar el pago de la cuota mensual de su ahorro.</p>
+                        <p className="text-gray-700 sm:text-xl">Aquí puede realizar el pago de la cuota mensual de su préstamo.</p>
                         <p className="text-gray-400">Complete el pago llenando la información de su método de pago.</p>
-                        <label htmlFor="card-holder" className="mt-4 mb-2 block text-sm font-medium">Titular de la tarjeta</label>
+                        <label htmlFor="cardholder" className="mt-4 mb-2 block text-sm font-medium">Titular de la tarjeta</label>
                         <div className="relative">
-                          <input type="text" id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Nombre completo del titular" />
+                          <input type="text" id="cardholder" name="cardholder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Nombre completo del titular" />
                           <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                               <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
                             </svg>
                           </div>
                         </div>
-                        <label htmlFor="card-no" className="mt-4 mb-2 block text-sm font-medium">Número de tarjeta</label>
+                        <label htmlFor="cardNo" className="mt-4 mb-2 block text-sm font-medium">Número de tarjeta</label>
                         <div className="relative w-7/12 flex-shrink-0">
-                          <input type="text" id="card-no" name="card-no" className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="xxxx-xxxx-xxxx-xxxx" />
+                          <input type="text" id="cardNo" name="cardNo" className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="xxxx-xxxx-xxxx-xxxx" />
                           <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                             <svg className="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="currentColor" viewBox="0 0 16 16">
                               <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
@@ -78,32 +86,33 @@ const PanelTarjeta = ({ router }) => {
                             </svg>
                           </div>
                         </div>
-                        <label htmlFor="card-no" className="mt-4 mb-2 block text-sm font-medium">Fecha de expiración</label>
+                        <label htmlFor="fechaDeExpiración" className="mt-4 mb-2 block text-sm font-medium">Fecha de expiración</label>
                         <input
-                          id="fecha de expiración"
-                          name="fecha de expiración"
+                          id="fechaDeExpiración"
+                          name="fechaDeExpiración"
                           type="date"
                           placeholder=""
                           className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" />
-                        <label htmlFor="cantidad_retirar" className="mt-4 mb-2 block text-sm font-medium">Código de seguridad</label>
+                        <label htmlFor="code" className="mt-4 mb-2 block text-sm font-medium">Código de seguridad</label>
                         <div className="relative">
-                          <input type="text" id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Ingrese el código de seguridad de su tarjeta" />
+                          <input type="text" id="code" name="code" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Ingrese el código de seguridad de su tarjeta" />
                           <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                               <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                             </svg>
                           </div>
                         </div>
-                        <label htmlFor="cantidad_retirar" className="mt-4 mb-2 block text-sm font-medium">Cantidad a pagar</label>
+                        <label htmlFor="cantidadPagar" className="mt-4 mb-2 block text-sm font-medium">Cantidad a pagar</label>
                         <div className="flex flex-col sm:flex-row">
                           <div className="relative flex-shrink-0 sm:w-7/12">
                             <input
                               onChange={handleChange}
                               type="text"
-                              id="cantidad_pago"
-                              name="cantidad_pago"
+                              id="cantidadPagar"
+                              name="cantidadPagar"
                               className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
                               placeholder=" "
+                              disabled
                             />
                             <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
                               <svg class="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -113,11 +122,11 @@ const PanelTarjeta = ({ router }) => {
                           </div>
                         </div>
                         <button
-                          onClick={handleSubmit}
-                          className="mt-4 mb-8 w- rounded-md bg-gradient-to-r from-sky-600 to-cyan-400 px-6 py-3 font-medium text-white"
-                        >
-                          Pagar
-                        </button>
+                          onClick={
+                            setShowModal(true)
+                          }
+                          className="mt-4 mb-8 w- rounded-md bg-blue-500 px-6 py-3 font-medium text-white">Pagar</button>
+                        {/*showModal && <PagoCuotaExitoso onClose={() => handleClose()}></PagoCuotaExitoso>*/}
                       </div>
 
                     )
@@ -139,4 +148,4 @@ const PanelTarjeta = ({ router }) => {
   )
 }
 
-export default withRouter(PanelTarjeta)
+export default withRouter(PanelAbonos)
