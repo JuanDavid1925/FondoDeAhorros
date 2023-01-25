eff4f3657804bad42eb7f6a3cb1c75a0e7457049
karenmoreno17 <karen.moreno@correounivalle.edu.co>
2023-01-25 05:58:26 -0500

    Reportes de admin

--- src/componentes/dashboard/admin/reportes/index.js
+++ src/componentes/dashboard/admin/reportes/index.js
@@ -1,13 +1,89 @@
 import Header_asociado from "/src/componentes/dashboard/compartido/header_asociado"
+import React from 'react'
+import { withRouter } from "next/router"
+import { TabHead, TabContainer, TabBody, Tab, Div } from "/src/componentes/dashboard/menuTabs"
+import Link from "next/link"
+import ReactDOM from 'react-dom'
+import AnyChart from 'anychart-react'
 
-export default function PanelReportes() {
+const PanelReportes = ({ router }) => {
 
+  const {
+    query: { tab }
+  } = router
+
+  const isTabOne = tab === "1" || tab == null
+  const isTabTwo = tab === "2"
+
+  const complexSettings = {
+    width: 700,
+    height: 450,
+    type: 'column',
+    data: "Ene,1\nFeb,3\nMar,4\nAbril,2\nMay,1\nJun,2\nJul,5\nAgos,2\nSep,3\nOctu,2\nNovi,5\nDici,1\n",/* Aquí se ingresan los datos del gráfico */
+    title: 'Meses con más registros de asociados:'
+  };
+
+  const complexSettingsClientes = {
+    width: 700,
+    height: 450,
+    type: 'column',
+    data: "Ene,3\nFeb,1\nMar,1\nAbril,2\nMay,4\nJun,2\nJul,7\nAgos,2\nSep,3\nOctu,4\nNovi,1\nDici,2\n", /* Aquí se ingresan los datos del gráfico */
+    title: 'Meses con más registros de clientes:'
+  };
 
   return (
     <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
       <Header_asociado nombre_seccion="Reportes" />
+      <TabContainer>
+        <TabHead>
+          <Tab selected={isTabOne}>
+            <Link href={{ query: { tab: "1" } }}>
+              <a>Reportes Asociados</a>
+            </Link>
+          </Tab>
+          <Tab selected={isTabTwo}>
+            <Link href={{ query: { tab: "2" } }}>
+              <a>Reportes Clientes</a>
+            </Link>
+          </Tab>
+        </TabHead>
+        <TabBody>
+          {isTabOne &&
+            <React.Fragment>
+              <div className="mt-10 bg-white px-12 pt-8 lg:mt-0 border-color-gray-600">
+                <p className="text-xl font-medium">Reportes sobre los asociados</p>
+                <p className="text-gray-400">Puede apreciar en los gráficos la información más importante sobre los asociados.</p>
+                <br></br>
+                <div>
+                  <AnyChart
+                    {...complexSettings}
+
+                  />
+                </div>
+              </div >
+            </React.Fragment>}
+          {isTabTwo &&
+            <React.Fragment>
+              <div className="mt-10 bg-white px-12 pt-8 lg:mt-0 border-color-gray-600">
+                <p className="text-xl font-medium">Reportes sobre los clientes</p>
+                <p className="text-gray-400">Puede apreciar en los gráficos la información más importante sobre los clientes.</p>
+                <br></br>
+                <div>
+                  <AnyChart
+                    {...complexSettingsClientes}
+
+                  />
+                </div>
+              </div >
+            </React.Fragment>}
+        </TabBody>
+      </TabContainer>
+
+
     </div >
 
   )
 
 }
+
+export default withRouter(PanelReportes)
--- src/pages/dashboard/asociado.js
+++ src/pages/dashboard/asociado.js
@@ -133,10 +133,9 @@ export default function Dashboard_Asociado() {
                   className={"group-hover:text-cyan-600" + ((mostrar_pagina === 6) ? "text-white group-hover:text-white" : "")}>Reuniones</span>
               </a>
             </li>
+            {/* DESCOMENTAR EN CASO DE TENER REPORTE DE ASOCIADOS
             <li>
               <a href="#" onClick={() => setEstadoPagina(7)} className={"px-4 py-3 flex items-center space-x-4 rounded-xl  group " + ((mostrar_pagina === 7) ? "text-white bg-gradient-to-r from-sky-600 to-cyan-400" : "text-gray-600")}>
-
-
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                   <path className={"fill-current text-gray-600 group-hover:text-cyan-600" + ((mostrar_pagina === 7) ? "text-white group-hover:text-white" : "")} fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd" />
                   <path className={"fill-current text-gray-300 group-hover:text-cyan-300" + ((mostrar_pagina === 7) ? "text-white group-hover:text-white" : "")} d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
@@ -146,7 +145,7 @@ export default function Dashboard_Asociado() {
                     setEstadoPagina(7)}
                   className={"group-hover:text-cyan-600" + ((mostrar_pagina === 7) ? "text-white group-hover:text-white" : "")}>Reportes</span>
               </a>
-            </li>
+            </li>*/}
           </ul>
         </div>
         <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
